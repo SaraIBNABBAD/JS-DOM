cfbd7d75cefec6c3687a0ceb966ce5f8d9c283f0
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-07 13:58:27 +0100

    COLOR

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ let count = Number(result.textContent);
 function addNumber(){
     console.log(count);
     result.textContent = ++count;
+    chnageColor();
+    
 }
-
+const reset = ()=>{
+    count=0;
+    result.textContent = count;
+    chnageColor();
+    
+};
 function lowNumber(){
     result.textContent = --count;
-    console.log(count);
+    chnageColor();
+    
 }
-
+function chnageColor(){
+    if(result.textContent>0){
+        result.style.color='blue'
+    }else if (result.textContent==0){
+        result.style.color='black'
+    } else
+        result.style.color='red'
+    
+}
 btns[0].addEventListener("click",addNumber); 
-btns[1].addEventListener("click",lowNumber); 
+btns[1].addEventListener("click",reset); 
+btns[2].addEventListener("click",lowNumber); 
