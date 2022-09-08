fc5e0b34ae64a2865f790725f15aea479ffe3f8b
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-08 10:54:49 +0100

    fist stape

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 
-let parg = document.getElementById("content");
-let icone = document.querySelector("button");
 
-icone.addEventListener("click",function(){
-    parg.classList.toggle("hide");
-    
-        icone.classList.toggle("active");
-    
-});
 
