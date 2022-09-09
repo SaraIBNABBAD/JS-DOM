e85e9d1cf07de06d10f67ff68a88371bf0c45353
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-09 05:19:35 +0100

    javascript

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
-let inputa = document.getElementById("taille");
-let inputb = document.getElementById("poids");
-let btn = document.querySelector("button");
-let result = document.querySelector("h2");
+ 
+ let choix = document.querySelectorAll("input");
 
-btn.addEventListener("click",function(){
-    let taille=Number(inputa.value)/100;
-    let poids=Number(inputb.value);
-    let res=(poids/(taille*taille)).toFixed(2);
-    console.log(res);
-    result.textContent=res
-})
+ choix[0].addEventListener("click",Devifac);
+ choix[1].addEventListener("click",Devimoy);
+ choix[2].addEventListener("click",Devidef);
+ function Aleatoire(min, max)
+ {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+ let entier = Aleatoire(1, 10);
+ console.log(entier);
+
+ function AleatoireA(min, max)
+ {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+ let entiera = AleatoireA(10, 100);
+ console.log(entiera);
+
+ function AleatoireB(min, max)
+ {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+ let entierb = AleatoireB(100, 1000);
+ console.log(entierb);
+
+ let nbr, chance;
+ chance = 3;
+
+
+function Devifac(){
+  switch (true) {
+        case (nbr > entier):
+          alert("plus petit");
+          chance--;
+          break;
+        case (nbr == entier):
+           alert("nombre trouvé");
+           chance = 0;
+           break;
+        default:
+          alert("plus grand");
+          chance--;
+       }
+};
+
+function Devimoy(){
+  switch (true) {
+        case (nbr > entiera):
+          alert("plus petit");
+          chance--;
+          break;
+        case (nbr == entiera):
+           alert("nombre trouvé");
+           chance = 0;
+           break;
+        default:
+          alert("plus grand");
+          chance--;
+       }
+};
+
+function Devidef(){
+  switch (true) {
+        case (nbr > entierb):
+          alert("plus petit");
+          chance--;
+          break;
+        case (nbr == entierb):
+           alert("nombre trouvé");
+           chance = 0;
+           break;
+        default:
+          alert("plus grand");
+          chance--;
+       }
+}
