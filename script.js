1e7922829a0ee607fe712cc801e37a2ceee9ebcd
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-15 22:26:13 +0100

    quote alea

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let space=document.getElementById("quote");
+
 let btn = document.querySelector("button");
+let phrase = document.querySelector(".phrase");
+let auteur = document.querySelector(".auteur");
 
-function getQuote(){
-  let quote=quote[Math.floor(Math.random() * quote.length)];
- space.innerText=quote;
-}
-btn.addEventListener("click",getQuote);
 
- let url="https://jsonplaceholder.typicode.com/users";
+ let url="https://type.fit/api/quotes";
+    async function getQuote(){
+     let quot = await fetch(url);
+     let quotee = await quot.json();
+     let num = Math.floor(Math.random() * quotee.length);
+     let texte=quotee[num];
+    console.log(texte);
+    phrase.innerText=texte.text;
+    auteur.innerText=texte.author;
+   }
+    getQuote(url);
+    btn.addEventListener("click",getQuote);
