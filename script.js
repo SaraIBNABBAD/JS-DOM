483e7404f8e0e44a1f52eceafe57274be4d5f46d
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-16 10:36:20 +0100

    correction

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ let btn = document.querySelector("button");
 let phrase = document.querySelector(".phrase");
 let auteur = document.querySelector(".auteur");
 
-
+let quotee
  let url="https://type.fit/api/quotes";
     async function getQuote(){
-     let quot = await fetch(url);
-     let quotee = await quot.json();
-     let num = Math.floor(Math.random() * quotee.length);
+      try {
+         let quot = await fetch(url);
+     quotee = await quot.json();
+      } catch (error) {
+         console.log(error);
+      }
+      randomQuote();
+   }
+    getQuote(url);
+
+    function randomQuote(){
+      let num = Math.floor(Math.random() * quotee.length);
      let texte=quotee[num];
     console.log(texte);
     phrase.innerText=texte.text;
     auteur.innerText=texte.author;
-   }
-    getQuote(url);
-    btn.addEventListener("click",getQuote);
+    }
+    btn.addEventListener("click",randomQuote);
