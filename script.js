b40482d30ab2697181315ddfe45fb91956b8bc2b
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-18 01:12:14 +0100

    dbl click

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let btns = document.querySelectorAll("button");
 let like = document.querySelector(".like");
 let dislike = document.querySelector(".dislike");
+let image = document.querySelector("img");
 
 let count=0;
 let num=0;
 btns[0].addEventListener("click",function(){
     count++;
-    like.textContent=count++;
+    like.textContent=count;
 })
 btns[1].addEventListener("click",function(){
     num++;
-    dislike.textContent=num++;
+    dislike.textContent=num;
+})
+image.addEventListener("dblclick",function(){
+    count++;
+    like.textContent=count;
 })
