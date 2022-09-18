0b688702e47377a05d123e2bb97231cc36db6846
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-18 01:54:39 +0100

    btn css

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let icone = document.querySelector("i");
 
 btn.addEventListener("click",function(){
     body.classList.toggle("dark");
-    if(body.classList.contains("dark")){
+    btn.classList.toggle("butn");
+    if(body.classList.contains("dark") && btn.classList.contains("butn")){
         icone.classList.replace("fa-moon","fa-sun");
     }else{
         icone.classList.replace("fa-sun","fa-moon");
