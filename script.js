8830b8a210a94fff05f239b1d7c108727a6f6277
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-18 18:43:31 +0100

    stockage

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ let dislike = document.querySelector(".dislike");
 let image = document.querySelector("img");
 
 let count=0;
-let num=0;
+let compte=0;
 btns[0].addEventListener("click",function(){
     count++;
     like.textContent=count;
 })
 btns[1].addEventListener("click",function(){
-    num++;
-    dislike.textContent=num;
+    compte++;
+    dislike.textContent=compte;
 })
 image.addEventListener("dblclick",function(){
     count++;
     like.textContent=count;
 })
+
+const sauvgarde = {
+    "lik" : count,
+    "dislk" : compte,
+}
+let save = JSON.stringify(sauvgarde);
+localStorage.setItem("sauvgarde",save);
