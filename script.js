a9e6dd62753353113df2010ee450b7648207d63c
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-18 21:01:00 +0100

    stockage

--- script.js
+++ script.js
@@ -8,19 +8,37 @@ let compte=0;
 btns[0].addEventListener("click",function(){
     count++;
     like.textContent=count;
+    if (localStorage.count) {
+        localStorage.count = Number(localStorage.count)+1;
+      } else {
+        localStorage.count = 1;
+      }
+      like.textContent = localStorage.count;
 })
 btns[1].addEventListener("click",function(){
     compte++;
     dislike.textContent=compte;
+    if (localStorage.compte) {
+        localStorage.compte = Number(localStorage.compte)+1;
+      } else {
+        localStorage.compte = 1;
+      }
+      dislike.textContent = localStorage.compte;
 })
 image.addEventListener("dblclick",function(){
     count++;
     like.textContent=count;
+    if (localStorage.count) {
+        localStorage.count = Number(localStorage.count)+1;
+      } else {
+        localStorage.count = 1;
+      }
+      like.textContent = localStorage.count;
 })
 
-const sauvgarde = {
-    "lik" : count,
-    "dislk" : compte,
-}
-let save = JSON.stringify(sauvgarde);
-localStorage.setItem("sauvgarde",save);
+// const sauvgarde = {
+//     "lik" : count,
+//     "dislk" : compte,
+// }
+// let save = JSON.stringify(sauvgarde);
+// localStorage.setItem("sauvgarde",save);
