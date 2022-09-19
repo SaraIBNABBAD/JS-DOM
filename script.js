daf162d49baaa94c93374466374518f483fe0831
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-19 10:07:16 +0100

    js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 
 let parg = document.getElementById("content");
-let icones = document.querySelectorAll("button");
+let icone = document.querySelectorAll("button");
 
-icones[0].addEventListener("click",function(){
-    parg.classList.toggle("hide");
-    
-        icones.classList.toggle("active");
-    
-});
-icones[2].addEventListener("click",function(){
+icone[0].addEventListener("click",liste);
+icone[2].addEventListener("click",liste);
+
+function liste(){
   parg.classList.toggle("hide");
-  
-      icones.classList.toggle("active");
-  
-});
-
+  icone.classList.toggle("active");
+}
