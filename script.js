5e6327e3a4a69322e0b3fd61558dcb77310e23c3
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-19 10:48:04 +0100

    fin

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let parag = document.getElementById("content");
-let icon = document.querySelector("button");
+let icon = document.querySelector("i");
 
 icon.addEventListener("click",function(){
     parag.classList.toggle("hide");
     icon.classList.toggle("active");
+    
 })
