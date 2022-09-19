160cdb15e3fc130a973be93de47f1af1d64dfb55
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-19 10:11:12 +0100

    dd

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let parg = document.getElementById("content");
 let icone = document.querySelectorAll("button");
 
 icone[0].addEventListener("click",liste);
-icone[2].addEventListener("click",liste);
+icone[1].addEventListener("click",liste);
 
 function liste(){
   parg.classList.toggle("hide");
