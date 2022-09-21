3417231d82f6cdd8cbc95b39d1939b4189c69ba0
sara ibnabbad <saraibnabbad96@gmail.com>
2022-09-21 09:23:03 +0100

    FUNCTION

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let parag = document.querySelector(".content");
-let icon = document.querySelector("div");
+let icon = document.querySelector("button");
 let symbol = document.querySelector("i");
-icon.forEach(div => {
-    icon[0].addEventListener("click",function(){
+icon.forEach(button => {
+    button.addEventListener("click",function(){
     parag.classList.toggle("hide");
     symbol.classList.toggle("active");
     });
